9b436ead764a0441a43f31c0711be0032726c616
Sarah Skull <112232523+SSkull1@users.noreply.github.com>
2024-08-28 12:03:26 +1000

    Create bt.py
    Communicate upstream to Raspberry Pi from Arduino

---
+++ bt.py
@@ -0,0 +1,19 @@
+import serial
+import time
+import string
+
+
+#reading and writing data from and to arduino serially
+#rfcomm0_> this could be different
+ser = serial.Serial("/dev/rfcomm0",9600,timeout=1)
+ser.reset_input_buffer()
+ser.write(str.encode('Start\r\n'))
+print("Test")
+
+while True:
+	#if ser.in_waiting>0:
+		rawserial = ser.readline()
+		print("Test again")
+		cookedserial=rawserial.decode('utf-8').strip('\r\n')
+		print(cookedserial)
+	
